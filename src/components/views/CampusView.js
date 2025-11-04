ec24401b71848783575c32921e2016cfb1a2aa96
Jia Jun Wu <jiajunwu041@gmail.com>
2025-11-04 05:13:37 -0500

    Added three buttons to add student, edit campus, delete student in single campus view

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -7,7 +7,7 @@ It constructs a React component to display a single campus and its students (if 
 import { Link } from "react-router-dom";
 import styles from "./CampusView.module.css"; // Import CSS module
 
-const CampusView = ({ campus }) => {
+const CampusView = ({ campus, deleteStudent }) => {
   if (!campus) return <div className={styles.noCampus}>Campus not found.</div>;
 
   return (
@@ -24,6 +24,15 @@ const CampusView = ({ campus }) => {
           <p className={styles.campusAddress}>{campus.address}</p>
           <p className={styles.campusDescription}>{campus.description}</p>
         </div>
+
+        <div className={styles.buttonContainer}>
+          <Link to="/newstudent" className={styles.buttonLink}>
+            <button className={styles.addStudentButton}>Add Student</button>
+          </Link>
+          <Link to={`/editcampus/${campus.id}`} className={styles.buttonLink}>
+            <button className={styles.editCampusButton}>Edit Campus</button>
+          </Link>
+        </div>
       </div>
 
       {/* Students Section */}
@@ -32,22 +41,30 @@ const CampusView = ({ campus }) => {
         {campus.students.length ? (
           <div className={styles.studentGrid}>
             {campus.students.map((student) => (
-              <Link
-                to={`/student/${student.id}`}
-                key={student.id}
-                className={styles.studentCard}
-              >
-                <img
-                  src={student.imageUrl || "https://cdn-icons-png.flaticon.com/512/3135/3135715.png"}
-                  alt={`${student.firstName} ${student.lastName}`}
-                  className={styles.studentImage}
-                />
-                <div className={styles.studentInfo}>
-                  <h3 className={styles.studentName}>
-                    {student.firstName} {student.lastName}
-                  </h3>
-                </div>
-              </Link>
+              <div key={student.id} className={styles.studentCardWrapper}>
+                <Link
+                  to={`/student/${student.id}`}
+                  key={student.id}
+                  className={styles.studentCard}
+                >
+                  <img
+                    src={student.imageUrl || "https://cdn-icons-png.flaticon.com/512/3135/3135715.png"}
+                    alt={`${student.firstName} ${student.lastName}`}
+                    className={styles.studentImage}
+                  />
+                  <div className={styles.studentInfo}>
+                    <h3 className={styles.studentName}>
+                      {student.firstName} {student.lastName}
+                    </h3>
+                  </div>
+                </Link>
+                <button
+                  className={styles.deleteStudentButton}
+                  onClick={() => deleteStudent(student.id)}
+                >
+                  &times;
+                </button>
+              </div>
             ))}
           </div>
         ) : (
